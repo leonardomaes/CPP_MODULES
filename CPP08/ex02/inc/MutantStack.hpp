81db3e3684d9d7ccf2b244db52f9a3c46e008bcc
lmaes <lmaes@Debian.myguest.virtualbox.org>
2025-09-12 18:00:54 +0100

    Added CPP08

--- CPP08/ex02/inc/MutantStack.hpp
+++ CPP08/ex02/inc/MutantStack.hpp
@@ -25,61 +25,25 @@
 #include <deque>
 #include <list>
 
-template <typename T> class MutantStack : public std::stack
+template <typename T>
+class MutantStack : public std::stack<T>
 {
 public:
 	MutantStack();
+	MutantStack& operator=(const MutantStack& obj);
+	MutantStack(const MutantStack& obj);
 	~MutantStack();
 
-	const std::stack<T>::container_type& getContainer() const;
-	std::stack<T>::container_type& getContainer();
+	typedef typename std::stack<T>::container_type container_type;
+	typedef typename container_type::iterator iterator;
+	typedef typename container_type::const_iterator const_iterator;
 
-	//Iterators
-	typedef std::deque<T>::const_iterator begin() const;
-	typedef std::deque<T>::const_iterator end() const;
-	
+	iterator begin();
+	iterator end();
+	const_iterator begin() const;
+	const_iterator end() const;
+
 };
-
-
-/*
-
-Elem access:
-empty() "Return true if the stack is empty"
-size()  "Return number of elements in stack"
-top()   "Return reference to the top element"
-
-Modif:
-push(const T& val)	"Add elem to the top"
-pop()				"Remove the top element"
-swap(stack& other)  "swap the content of 2 stacks"
-
-Comparations:
-"==", "!="
-"<", "<=", ">", ">="
-
-
-template <typename T, typename Container = std::deque<T> >
-class stack {
-protected:
-    Container c;        // o container real que armazena os elementos
-public:
-    typedef typename Container::value_type value_type;
-    typedef typename Container::size_type  size_type;
-
-    stack() : c() {}
-    explicit stack(const Container& cont) : c(cont) {}
-    bool empty() const { return c.empty(); }
-    size_type size() const { return c.size(); }
-    value_type& top() { return c.back(); }
-    const value_type& top() const { return c.back(); }
-    void push(const value_type& v) { c.push_back(v); }
-    void pop() { c.pop_back(); }
-    void swap(stack& other) { c.swap(other.c); }
-    // operadores de comparação normalmente delegam ao container c
-};
-
-
-*/
 
 
 #include "../src/MutantStack.tpp"
--- CPP08/ex02/src/MutantStack.tpp
+++ CPP08/ex02/src/MutantStack.tpp
@@ -12,12 +12,45 @@
 
 #include "../inc/MutantStack.hpp"
 
-template <class T>
-MutantStack<T>::MutantStack()
+template <typename T>
+MutantStack<T>::MutantStack() { }
+
+template <typename T>
+MutantStack<T>& MutantStack<T>::operator=(const MutantStack& obj)
 {
+	if (this != &obj)
+	{
+		std::stack<T>::operator=(obj);
+	}
+	return *this;
 }
 
-template <class T>
-MutantStack<T>::~MutantStack()
+template <typename T>
+MutantStack<T>::MutantStack(const MutantStack<T>& obj) : std::stack<T>(obj) { }
+
+template <typename T>
+MutantStack<T>::~MutantStack() { }
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::begin()
 {
+	return this->c.begin();
+}
+
+template <typename T>
+typename MutantStack<T>::iterator MutantStack<T>::end()
+{
+	return this->c.end();
+}
+
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::begin() const
+{
+	return this->c.begin();
+}
+
+template <typename T>
+typename MutantStack<T>::const_iterator MutantStack<T>::end() const
+{
+	return this->c.end();
 }
