721f410e5e1b93f44eea89611169a1952bea954b
lmaes <lmaes@Debian.myguest.virtualbox.org>
2025-10-02 15:36:22 +0100

    Added CPP09

---
+++ CPP09/ex02/inc/PmergeMe.hpp
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   PmergeMe.hpp                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lmaes <lmaes@student.42porto.com>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/29 19:36:46 by lmaes             #+#    #+#             */
+/*   Updated: 2025/09/29 19:36:47 by lmaes            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+
+#include <algorithm>
+#include <cctype>
+#include <cmath>
+#include <ctime>
+#include <cstdlib>
+#include <cstring>
+#include <exception>
+#include <fstream>
+#include <limits>
+#include <iostream>
+#include <iomanip>
+#include <iterator>
+#include <sstream>
+#include <string>
+#include <unistd.h>
+
+// Containers
+#include <deque>
+#include <vector>
+#include <stack>
+#include <list>
+#include <map>
+
+// Colors
+#define RESET   "[0m"
+#define BLACK   "[30m"
+#define RED     "[31m"
+#define GREEN   "[32m"
+#define YELLOW  "[33m"
+#define BLUE    "[34m"
+#define MAGENTA "[35m"
+#define CYAN    "[36m"
+#define WHITE   "[37m"
+
+// Background colors
+#define BCK_BLACK   "[40m"
+#define BCK_RED     "[41m"
+#define BCK_GREEN   "[42m"
+#define BCK_YELLOW  "[43m"
+#define BCK_BLUE    "[44m"
+#define BCK_MAGENTA "[45m"
+#define BCK_CYAN    "[46m"
+#define BCK_WHITE   "[47m"
